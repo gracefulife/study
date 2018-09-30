34921b22673f57e4b25d39abc9593b9b510e7d4e
gracefulife <gracefulife@naver.com>
2018-09-30 23:09:08 +0900

    add WritePostService specs

---
+++ blog/src/test/groovy/WritePostServiceSpecs.groovy
@@ -0,0 +1,22 @@
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class WritePostServiceSpecs extends Specification {
+
+    def setup() {
+        // TODO not yet implements
+    }
+
+    def '글은 제목과 내용이 모두 입력되어야 합니다.'() {
+        // TODO not yet implements
+    }
+
+    def '글 제목이 등록되지 않은 경우 IllegalArgumentException 이 발생되어야 합니다.'() {
+        // TODO not yet implements
+    }
+
+    def '글 내용이 등록되지 않은 경우 IllegalArgumentException 이 발생되어야 합니다.'() {
+        // TODO not yet implements
+    }
+}
