c4673fb56a7a713244f83711869e852690f688c4
gracefulife <gracefulife@naver.com>
2018-11-04 22:21:56 +0900

    datasource 설정
    reactive mongo

---
+++ blog/src/main/java/xyz/gracefulife/blog/DataSourceConfig.java
@@ -0,0 +1,28 @@
+package xyz.gracefulife.blog;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
+@EnableReactiveMongoRepositories
+@AllArgsConstructor
+public class DataSourceConfig extends AbstractReactiveMongoConfiguration {
+  private MongoProperties mongoProperties;
+
+  @Override public MongoClient reactiveMongoClient() {
+    return MongoClients.create();
+  }
+
+  @Override protected String getDatabaseName() {
+    return mongoProperties.getDatabase();
+  }
+}
