3723879d35790c8e97dd75ce4163e4da25de5e99
gracefulife <gracefulife@naver.com>
2018-11-04 22:29:07 +0900

    삭제, 업데이트 API 추가

--- blog/src/main/java/xyz/gracefulife/blog/PostController.java
+++ blog/src/main/java/xyz/gracefulife/blog/PostController.java
@@ -1,8 +1,12 @@
 package xyz.gracefulife.blog;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
@@ -10,18 +14,36 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping("/posts")
 @AllArgsConstructor
 public class PostController {
   private final ReadPostService readPostService;
   private final WritePostService writePostService;
+  private final UpdatePostService updatePostService;
+  private final DeletePostService deletePostService;
 
-  @GetMapping("/posts")
-  public Flux<PostResponse> posts() {
+  @GetMapping
+  public Flux<PostResponse> readPosts() {
     return readPostService.findAll();
   }
 
-  @PostMapping("/posts")
-  public Mono<PostResponse> savePost(@RequestBody Post post) {
+  @GetMapping("/{id}")
+  public Mono<PostResponse> readPost(@PathVariable String id) {
+    return readPostService.findById(id);
+  }
+
+  @PostMapping
+  public Mono<PostResponse> createPost(@RequestBody Post post) {
     return writePostService.write(post);
+  }
+
+  @PutMapping
+  public Mono<PostResponse> updatePost(@RequestBody Post post) {
+    return updatePostService.update(post);
+  }
+
+  @DeleteMapping("/{id}")
+  public Mono<String> deletePost(@PathVariable String id) {
+    return deletePostService.delete(id);
   }
 }
